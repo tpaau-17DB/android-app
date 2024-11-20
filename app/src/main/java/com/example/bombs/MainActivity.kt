bbc97deaa8f7b5ccc84cade2433ad5bf9d900d64
tpaau-17DB <mikolaj_archusr@tutamail.com>
2024-11-20 19:50:49 +0100

    Added the project files.

---
+++ app/src/main/java/com/example/bombs/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.bombs
+
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.bombs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
